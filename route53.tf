4846bc2c8c634f1a11487c58a0aeef7b30a1d0ac
Patrick Downey <patrickdowney@dioadconsulting.com>
2019-09-10 22:00:15 +0100

    Import terraform module from github.com/dioadconsulting/dioad-terraform-modules

---
+++ route53.tf
@@ -0,0 +1,37 @@
+resource "aws_route53_record" "txt_amazonses" {
+  zone_id = "${var.zone_id}"
+
+  name    = "_amazonses"
+  type    = "TXT"
+  ttl     = "300"
+  records = ["${var.domain_verification}"]
+}
+
+resource "aws_route53_record" "mx" {
+  zone_id = "${var.zone_id}"
+
+  name    = ""
+  type    = "MX"
+  ttl     = "86400"
+  records = ["10 inbound-smtp.${var.region}.amazonaws.com."]
+}
+
+resource "aws_route53_record" "cname_autodiscover" {
+  zone_id = "${var.zone_id}"
+
+  name    = "autodiscover"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["autodiscover.mail.${var.region}.awsapps.com."]
+}
+
+resource "aws_route53_record" "cname_dkim" {
+  zone_id = "${var.zone_id}"
+
+  name    = "${element(var.domain_keys, count.index)}._domainkey"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${element(var.domain_keys, count.index)}.dkim.amazonses.com."]
+
+  count = "${length(var.domain_keys)}"
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "zone_id" {}
+
+variable "region" {}
+
+variable "domain_verification" {}
+
+variable "domain_keys" {
+  type = "list"
+}
